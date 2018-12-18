abada957c2cd66686c13e2e1f82b54142200b74f
dingyang <42425445+sjtupig@users.noreply.github.com>
2018-12-18 10:07:32 +0800

    Add files via upload

---
+++ 023-从上往下打印二叉树.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回从上到下每个节点值列表，例：[1,2,3]
+    def PrintFromTopToBottom(self, root):
+        # write code here
+        if not root: #若为空返回空列表
+            return []
+        res = []#返回值
+        deque = [] #队列保存结点
+        deque.append(root)
+        while deque:
+            t = deque[0]
+            del deque[0] #出队
+            if t.left:
+                deque.append(t.left)
+            if t.right:
+                deque.append(t.right)
+            res.append(t.val)
+        return res
