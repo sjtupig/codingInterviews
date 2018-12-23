4d6c6c61e12145fa189341ae05f623d91575c412
dingyang <42425445+sjtupig@users.noreply.github.com>
2018-12-23 11:49:31 +0800

    Update 037-两个链表的第一个公共结点.py

--- 037-两个链表的第一个公共结点.py
+++ 037-两个链表的第一个公共结点.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-#以下解法默认链表无环
+#以下解法默认链表无环,如果有公共元素，会形成Y字形
 class ListNode:
     def __init__(self, x):
         self.val = x
@@ -44,3 +44,18 @@ c.next=a
 
 sol=Solution()
 print(sol.FindFirstCommonNode(pHead1,pHead2).val)
+
+#还可以使用栈来做，这样栈顶元素为两个链表的尾巴，如果相交，则栈顶必定一样
+#还可以使用hash表的方法
+class Solution:
+    def FindFirstCommonNode(self, pHead1, pHead2):
+        # write code here
+        maps = {}
+        while pHead1:
+            maps[pHead1] = 1
+            pHead1 = pHead1.next
+        while pHead2:
+            if pHead2 in maps:
+                return pHead2
+            pHead2 = pHead2.next
+        return 
