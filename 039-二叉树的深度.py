44ff72c921c9fb921f3dc45f2a692cc653964379
dingyang <42425445+sjtupig@users.noreply.github.com>
2018-12-24 15:48:31 +0800

    Add files via upload

---
+++ 039-二叉树的深度.py
@@ -0,0 +1,62 @@
+# -*- coding:utf-8 -*-
+#这里计算的是结点的个数
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+class Solution:
+    def __init__(self):
+        #统计结点的个数，最后-1
+        self.max_len = 0
+        self.node_nums = 0
+    def TreeDepth(self, pRoot):
+        # write code here
+        if not pRoot:return 0
+        self.node_nums += 1
+        if pRoot:
+            print('pRoot.val',pRoot.val)
+        print('self.node_nums',self.node_nums)
+        if not pRoot.left and not pRoot.right:
+            self.max_len = max(self.max_len, self.node_nums)
+        if pRoot.left:
+            self.TreeDepth(pRoot.left)
+            self.node_nums -=1
+        if pRoot.right:
+            self.TreeDepth(pRoot.right)
+            self.node_nums -= 1
+            
+        return self.max_len
+
+
+
+class Solution:
+    def TreeDepth(self, pRoot):
+        if pRoot==None:return 0
+        return max(self.TreeDepth(pRoot.left),self.TreeDepth(pRoot.right))+1
+
+
+#除此以外，还可以用层次遍历的方法来写，这里用队列来做，，前面写过类似代码，后面有时间再补
+
+a = TreeNode(1)
+b = TreeNode(2)
+c = TreeNode(3)
+d = TreeNode(4)
+e = TreeNode(5)
+f = TreeNode(6)
+g = TreeNode(7)
+h = TreeNode(8)
+'''a.left = b
+ a.right = c 
+ b.left = d
+ b.right = e 
+ e.left = g 
+ c.right = f 
+ f.right = h''' 
+a.right = b
+b.right = c 
+c.right = d 
+d.right = e 
+
+sol = Solution()
+print(sol.TreeDepth(a))
