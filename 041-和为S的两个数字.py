da0f61a814a2fb6e04539baa590d2fa58efa0dcd
dingyang <42425445+sjtupig@users.noreply.github.com>
2018-12-25 09:58:55 +0800

    从两端往里走，乘积一定是越来越大

--- 041-和为S的两个数字.py
+++ 041-和为S的两个数字.py
@@ -10,13 +10,16 @@ class Solution:
             if array[l]+array[r] > tsum:
                 r-=1
             if array[l]+array[r] == tsum:
+                #找到第一队可以直接返回，因为这个乘积一定最小，1*6<2*3
+                '''
                 if not res:
                     res=[array[l],array[r]]
                 else:
                     if array[l]*array[r] < res[0]*res[1]:
                         res=[array[l],array[r]]
                 l+=1
-                r-=1
+                r-=1'''
+                return [array[l],array[r]]
         return res
 
 
